0a21a815f980fc2dfb0dc00b3a5b15319d743ed3
brianpavon <brian.pavon@hotmail.com>
2022-04-07 01:20:26 -0300

    dejo ok el login basico, ruteo ok

--- src/app/Class/User/user.ts
+++ src/app/Class/User/user.ts
@@ -1,2 +1,26 @@
+
 export class User {
+    name:string;
+    password:string;    
+    
+    /**
+     *
+     */
+    constructor() {
+        this.name = '';
+        this.password = '';
+        
+    }
+
+    validate(){
+        if(this.name != 'Brian' || this.password != '1123'){
+            console.log(`nombre: ${this.name} pass: ${this.password}. El usuario es Brian y la pass 1123`);
+            
+            confirm('Usuario invalido.');            
+        }else{
+            confirm('Login correcto');
+            this.name = '';
+            this.password = '';
+        }
+    }
 }
