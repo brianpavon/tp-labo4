5e588d6942d03633558f43ea90860e59383ccf52
brianpavon <brian.pavon@hotmail.com>
2022-04-07 20:15:53 -0300

    agrego ruteo a pagina de error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EjercicioUnoComponent } from './page/ejercicioUno/ejercicio-uno.component';
+import { ErrorComponent } from './page/error/error.component';
 import { HomeComponent } from './page/home/home.component';
 import { LoginComponent } from './page/login/login.component';
 import { QuienSoyComponent } from './page/quien-soy/quien-soy.component';
@@ -14,7 +15,13 @@ const routes: Routes = [
   {path: 'ingreso', component:LoginComponent},
   {path: 'quien-soy', component:QuienSoyComponent},
   {path: 'ejercicio-uno', component:EjercicioUnoComponent},
-  {path: '',redirectTo: 'home',pathMatch: 'full'}
+  //esto si quiero una pagina dentro de otra, en el padre hay que poner el router outlet
+  //{path:'juegos',component:componentJuego children: [
+    //{path:'tateti',component:tateticomponent},
+    //{path: '**', component:ErrorComponent}
+  //]},
+  {path: '',redirectTo: 'home',pathMatch: 'full'},
+  {path: '**', component:ErrorComponent}
 
 ];
 
