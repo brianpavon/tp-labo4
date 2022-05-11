4366310351689a496b1642e460716e8195aaacc8
brianpavon <brian.pavon@hotmail.com>
2022-05-11 04:05:02 -0300

    agrego mensajes si perdes y ganas

--- src/app/juegos/pages/ahorcado/ahorcado.component.ts
+++ src/app/juegos/pages/ahorcado/ahorcado.component.ts
@@ -12,7 +12,8 @@ export class AhorcadoComponent implements OnInit {
   intentos:number = 0;  
   palabraRandom:string = '';
   palabraElegida:string[]=[];
-  palabraCodificada:string='';  
+  palabraCodificada:string='';
+  cantLetras:number = 0;
   constructor() { 
     this.letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   }
@@ -23,10 +24,10 @@ export class AhorcadoComponent implements OnInit {
   cargarJuego(){
     this.intentos = 6;
     const quePalabra = Math.floor(Math.random() * (this.palabras.length -1));
-    console.log(quePalabra);
+    //console.log(quePalabra);
     
     this.palabraRandom = this.palabras[quePalabra];
-
+    this.cantLetras = this.palabraRandom.length;
     //console.log(`longitud: ${this.palabraRandom.length}`);
     for (let i = 0; i < this.palabraRandom.length; i++) {
       //console.log(`letra: ${this.palabraRandom[i]}`);
@@ -43,26 +44,45 @@ export class AhorcadoComponent implements OnInit {
         //console.log(this.palabraCodificada[i]);
         if(this.palabraRandom[i] == letra){
           this.palabraElegida[i] = letra;
+          this.cantLetras--;
         }
       } 
       this.palabraCodificada = this.palabraElegida.join(' ');     
     }else{
       this.intentos--;
     }
+    if(this.cantLetras == 0)  this.winner();
     if(this.intentos == 0)  this.loser();
   }
 
   loser(){
     Swal.fire({
       title: 'Perdiste, la proxima quizas ganas',
-      icon: 'error',
+      icon: 'warning',
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Aceptar',
     });
+    this.palabraRandom = '';
+    this.palabraElegida=[];
+    this.palabraCodificada='';    
+    this.cantLetras = 0;
     this.cargarJuego();
   }
-
+  winner(){
+    Swal.fire({
+      title: 'Ganaste sos re piolita',
+      icon: 'success',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar'      
+    });
+    this.palabraRandom = '';
+    this.palabraElegida=[];
+    this.palabraCodificada='';    
+    this.cantLetras = 0;
+    this.cargarJuego();
+  }
 
 
 }
